38f2cc02af01a5464dd4ffb7597c4fd2b111c1f9
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-12-01 09:21:54 -0500

    initial commit

---
+++ config/config.exs
@@ -0,0 +1,14 @@
+import Config
+import Dotenvy
+
+source!([".env", System.get_env()])
+
+config :advent_of_code_utils,
+  session: System.get_env("ADVENT_SESSION"),
+  auto_compile?: true,
+  time_calls?: true,
+  gen_tests?: true,
+  year: 2024,
+  day: 1
+
+config :iex, inspect: [charlists: :as_lists]
