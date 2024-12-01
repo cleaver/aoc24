1a687d7eb7f45f85ab358239902490f961706b2f
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-12-01 12:35:14 -0500

    day 1

---
+++ lib/2024/1.ex
@@ -0,0 +1,44 @@
+import AOC
+
+import InputHelpers
+
+aoc 2024, 1 do
+  def p1(input) do
+    {list1, list2} =
+      input
+      |> parse_input()
+      |> parse_lists()
+
+    list_difference(Enum.sort(list1), Enum.sort(list2))
+    |> Enum.sum()
+  end
+
+  defp parse_lists(input) do
+    Enum.reduce(input, {[], []}, fn line, {acc1, acc2} ->
+      [_, first, second] = Regex.run(~r/(\d+)\s+(\d+)/, line)
+      {acc1 ++ [String.to_integer(first)], acc2 ++ [String.to_integer(second)]}
+    end)
+  end
+
+  defp list_difference([a | rest1], [b | rest2]) do
+    difference = abs(a - b)
+    [difference | list_difference(rest1, rest2)]
+  end
+
+  defp list_difference([], []) do
+    []
+  end
+
+  def p2(input) do
+    {list1, list2} =
+      input
+      |> parse_input()
+      |> parse_lists()
+
+    hist_list_2 = Enum.reduce(list2, %{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+
+    list1
+    |> Enum.map(&(&1 * Map.get(hist_list_2, &1, 0)))
+    |> Enum.sum()
+  end
+end
